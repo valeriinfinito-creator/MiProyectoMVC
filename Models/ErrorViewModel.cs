65fcaa6f98656960c1706883cab64ecc30f3544e
valeria <valeriinfinito@gmail.com>
2026-04-15 22:49:42 -0500

    se sube MiProtyectoMVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiProyectoMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
